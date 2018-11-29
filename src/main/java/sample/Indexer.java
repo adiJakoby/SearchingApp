922e54c41cec96efb0f4393cfe3b6e8e6598d5d0
adiJakoby <adij94@gmail.com>
2018-11-29 20:01:25 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/sample/Indexer.java

--- src/main/java/sample/Indexer.java
+++ src/main/java/sample/Indexer.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.util.*;
 
 public class Indexer {
-    //globals
     static int postingIndex = 0;
     private static TreeMap<String, String> mergedTerms = new TreeMap<>(new Indexer.MyComp2());
     private static HashSet<String> dictionary = new HashSet<>();
@@ -15,8 +14,9 @@ public class Indexer {
     /**
      * get a sorted map of tokens of doc, add the tokens to the generic dictionary of the corpus, and every few calls to
      * this function write to the disk the temporally posting files.
-     * @param tokens - the given map of a docName doc
-     * @param docName - the name of the doc.
+     *
+     * @param tokens   - the given map of a docName doc
+     * @param docName  - the name of the doc.
      * @param doneFile - if a specific file is done.
      */
     public void indexing(TreeMap<String, Integer> tokens, String docName, boolean doneFile) {
@@ -24,7 +24,8 @@ public class Indexer {
         if (doneFile) {
             doneCounter++;
         }
-        if((doneCounter == 33)){
+
+        if (doneCounter == 33) {
             addToMap(tokens, docName);
             if (mergedTerms.size() > 0) {
                 executePosting();
@@ -37,20 +38,20 @@ public class Indexer {
 
     /**
      * adding the map of the tokens of a doc to the local map of a few docs. merge the tokens map.
+     *
      * @param tokens
      * @param docName
      */
-    private void addToMap(TreeMap<String, Integer> tokens, String docName){
-        while(tokens.size() > 0){
+    private void addToMap(TreeMap<String, Integer> tokens, String docName) {
+        while (tokens.size() > 0) {
             String token = tokens.firstKey();
             int counter = tokens.pollFirstEntry().getValue();
-            if (mergedTerms.containsKey(token)){
+            if (mergedTerms.containsKey(token)) {
                 StringBuilder temp = new StringBuilder();
                 temp.append(mergedTerms.get(token));
                 temp.append(" " + docName + " " + counter);
                 mergedTerms.put(token, temp.toString());
-            }
-            else{
+            } else {
                 mergedTerms.put(token, docName + " " + counter);
             }
         }
@@ -61,10 +62,10 @@ public class Indexer {
      */
     private void executePosting() {
         try {
-            PrintWriter writer = new PrintWriter("C:\\Users\\tzalach\\IdeaProjects\\SearchingApp\\src\\main\\java\\" + postingIndex + ".txt", "UTF-8");
+            PrintWriter writer = new PrintWriter("C:\\Users\\adijak\\IdeaProjects\\SearchingApp\\src\\main\\java\\" + postingIndex + ".txt", "UTF-8");
             StringBuilder toWrite = new StringBuilder();
-            while(mergedTerms.size() > 0){
-                toWrite.append(mergedTerms.firstKey() + " " + mergedTerms.pollFirstEntry().getValue() + '\n');
+            while (mergedTerms.size() > 0) {
+                toWrite.append(mergedTerms.firstKey() + ": " + mergedTerms.pollFirstEntry().getValue() + '\n');
             }
             writer.print(toWrite);
             writer.close();
@@ -90,8 +91,9 @@ public class Indexer {
      */
     public void mergePostingFile() {
         try {
+            int pointer = 0;
             // open a buffer writer to the final posting file
-            FileWriter fw = new FileWriter("C:\\Users\\tzalach\\IdeaProjects\\SearchingApp\\src\\main\\java\\Posting.txt");
+            FileWriter fw = new FileWriter("C:\\Users\\adijak\\IdeaProjects\\SearchingApp\\src\\main\\java\\Posting.txt");
             BufferedWriter WriteFileBuffer = new BufferedWriter(fw);
             // open a buffer reader to each temp posting file
             BufferedReader[] bufferReaders = new BufferedReader[postingIndex];
@@ -102,7 +104,7 @@ public class Indexer {
 
             //initial the tree map terms with the X first lines.
             for (int i = 0; i < postingIndex; i++) {
-                File file = new File("C:\\Users\\tzalach\\IdeaProjects\\SearchingApp\\src\\main\\java\\" + i + ".txt");
+                File file = new File("C:\\Users\\adijak\\IdeaProjects\\SearchingApp\\src\\main\\java\\" + i + ".txt");
                 FileReader fileReader = new FileReader(file);
                 bufferReaders[i] = new BufferedReader(fileReader);
                 String line;
@@ -119,8 +121,14 @@ public class Indexer {
 
             //running over the the tree map with 3 term from each temp posting file. and writing to the final posting file every 10 terms.
             String currentTerm = terms.firstKey();
-            int index = currentTerm.indexOf(" ");
+            int index = currentTerm.indexOf(":");
             currentTerm = currentTerm.substring(0, index);
+            if (Character.isUpperCase(currentTerm.charAt(0))){
+                if(dictionary.contains(currentTerm.toLowerCase())){
+                    dictionary.remove(currentTerm);
+                    currentTerm = currentTerm.toLowerCase();
+                }
+            }
             toWrite.append(terms.firstKey());
             String lastTerm = currentTerm;
             int j = terms.pollFirstEntry().getValue();
@@ -128,18 +136,25 @@ public class Indexer {
             int counter = 1;
             while (terms.size() > 0) {
                 currentTerm = terms.firstKey();
-                index = currentTerm.indexOf(" ");
+                index = currentTerm.indexOf(":");
                 currentTerm = currentTerm.substring(0, index);
+                if (Character.isUpperCase(currentTerm.charAt(0))){
+                    if(dictionary.contains(currentTerm.toLowerCase())){
+                        dictionary.remove(currentTerm);
+                        currentTerm = currentTerm.toLowerCase();
+                    }
+                }
+
                 if (currentTerm.equals(lastTerm)) {
-                    toWrite.append(terms.firstKey().substring(index));
+                    toWrite.append(terms.firstKey().substring(index+1));
                 } else {
                     if (counter == 10) {
                         writeToPosting(toWrite, WriteFileBuffer);
                         toWrite = new StringBuilder();
-                        toWrite.append("\n" + terms.firstKey());
+                        toWrite.append("\n" + currentTerm + terms.firstKey().substring(index));
                         counter = 1;
                     } else {
-                        toWrite.append("\n" + terms.firstKey());
+                        toWrite.append("\n" + currentTerm + terms.firstKey().substring(index));
                         counter++;
                     }
                     lastTerm = currentTerm;
@@ -155,11 +170,13 @@ public class Indexer {
         } catch (IOException Ex) {
             System.out.println(Ex.getMessage());
         }
+        System.out.println("we have: " + dictionary.size() + " terms");
     }
 
     /**
      * checks if the tree map of the terms contain at least 1 term from the temp posting file (file Index)
      * if not, add 3 terms fron this temp posting file to the tree map.
+     *
      * @param fileIndex
      * @param bufferedReader
      */
@@ -170,13 +187,13 @@ public class Indexer {
                 String line;
                 int counter = 1;
                 line = bufferedReader.readLine();
-                if(line != null) {
+                if (line != null) {
                     terms.put(line, fileIndex);
                     howMuchTerms[fileIndex]++;
                 }
                 while (line != null && counter != 3) {
                     line = bufferedReader.readLine();
-                    if(line != null) {
+                    if (line != null) {
                         terms.put(line, fileIndex);
                         howMuchTerms[fileIndex]++;
                         counter++;
@@ -190,6 +207,7 @@ public class Indexer {
 
     /**
      * writing to the final posting file the string to write
+     *
      * @param toWrite
      * @param WriteFileBuffer
      */
@@ -207,16 +225,16 @@ public class Indexer {
     class MyComp implements Comparator<String> {
         @Override
         public int compare(String s1, String s2) {
-            int index1 = s1.indexOf(" ");
-            int index2 = s2.indexOf(" ");
+            int index1 = s1.indexOf(":");
+            int index2 = s2.indexOf(":");
             String s11 = s1.substring(0, index1);
             String s22 = s2.substring(0, index2);
             String s1l = s11.toLowerCase();
             String s2l = s22.toLowerCase();
             if (s1l.equals(s2l)) {
-                if(s11.equals(s22)){
+                if (s11.equals(s22)) {
                     return s1.compareTo(s2);
-                }else{
+                } else {
                     return s1.compareTo(s2);
                 }
 
@@ -230,7 +248,7 @@ public class Indexer {
         public int compare(String s1, String s2) {
             String s1l = s1.toLowerCase();
             String s2l = s2.toLowerCase();
-            if(s1l.equals(s2l)){
+            if (s1l.equals(s2l)) {
                 return s1.compareTo(s2);
             }
             return s1l.compareTo(s2l);
