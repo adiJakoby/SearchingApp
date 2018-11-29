fa4cfc6da9040c35371170713e34e236d26f5101
tzalach <tzalach@post.bgu.ac.il>
2018-11-29 20:04:27 +0200

    testing writer Cities indexer

--- src/main/java/sample/CitiesIndexer.java
+++ src/main/java/sample/CitiesIndexer.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.Vector;
 
 
 public class CitiesIndexer {
@@ -32,10 +33,7 @@ public class CitiesIndexer {
 
     //connecting to the cities API and get all cities details into cities hashmap
     public void api_Connection() {
-        //int maxlength=0;
-        //String maxCity="";
         OkHttpClient myClient = new OkHttpClient();
-        //HttpUrl.Builder urlBuilder = HttpUrl.parse("https://restcountries.eu/rest/v2/all?fielss=capital;name;pop;");
         String url = "https://restcountries.eu/rest/v2/all?fielss=capital;name;population;currency";
         Request req = new Request.Builder().url(url).build();
         Response res = null;
@@ -67,11 +65,6 @@ public class CitiesIndexer {
                 for (Object obj : theArray) {
                     coin = (String) ((JSONObject) obj).get("code");
                 }
-                /*
-                if(maxlength<capital.length()){
-                    maxlength=capital.length();
-                    maxCity=capital;
-                }*/
                 population = (Long) ((JSONObject) O).get("population");
                 String pop = divideNumbers(population);
                 citiesDetails.put(capital.toUpperCase(), new City(capital.toUpperCase(), country, coin, pop));
@@ -84,24 +77,75 @@ public class CitiesIndexer {
 
     //check if the city name is one word or to and add to the city dictionary the doc that contain the city and it location
     public void addCityToCorpusMap(String capital, String doc) {
-        String[] allCapitalParts = capital.split(" ");
-        if (allCapitalParts.length > 1) {
-            if(allCapitalParts.length == 3) {
-                if (citiesDetails.containsKey(allCapitalParts[0] + " " + allCapitalParts[1] + " " + allCapitalParts[2].toUpperCase())) {
-                    cities.put(doc, citiesDetails.get(allCapitalParts[0] + " " + allCapitalParts[1] + " " + allCapitalParts[2].toUpperCase()));
-                }
-            }
-            else if(allCapitalParts.length == 2) {
-                if (citiesDetails.containsKey(allCapitalParts[0] + " " + allCapitalParts[1].toUpperCase())) {
-                    cities.put(doc, citiesDetails.get(allCapitalParts[0] + " " + allCapitalParts[1].toUpperCase()));
-                }
-            }
+        char[] mychars = {'(', ')'};
+        if (capital.contains("(")) {
+            capital = OurReplace(capital, mychars, "");
         }
-        else{
+        String[] allCapitalParts = mySplit(capital," ");
+        if (allCapitalParts.length > 1) {
+            if (allCapitalParts.length == 3) {
+                if (citiesDetails.containsKey((allCapitalParts[0] + " " + allCapitalParts[1] + " " + allCapitalParts[2]).toUpperCase())) {
+                    cities.put(doc, citiesDetails.get((allCapitalParts[0] + " " + allCapitalParts[1] + " " + allCapitalParts[2]).toUpperCase()));
+                    System.out.println(doc);
+                } else {
+                    getCityDetails((allCapitalParts[0] + " " + allCapitalParts[1] + " " + allCapitalParts[2]).toUpperCase(), doc);
+                    System.out.println(doc);
+                }
+            } else if (allCapitalParts.length == 2) {
+                if (citiesDetails.containsKey((allCapitalParts[0] + " " + allCapitalParts[1]).toUpperCase())) {
+                    cities.put(doc, citiesDetails.get((allCapitalParts[0] + " " + allCapitalParts[1]).toUpperCase()));
+                    System.out.println(doc);
+                } else {
+                    getCityDetails((allCapitalParts[0] + " " + allCapitalParts[1]).toUpperCase(), doc);
+                    System.out.println(doc);
+                }
+            }
+        } else if (citiesDetails.containsKey(allCapitalParts[0].toUpperCase())) {
             cities.put(doc, citiesDetails.get(allCapitalParts[0].toUpperCase()));
+            System.out.println(doc);
+        } else {
+            getCityDetails(allCapitalParts[0].toUpperCase(), doc);
+            System.out.println(doc);
         }
     }
 
+    //in case that the city it not a capital
+    private void getCityDetails(String city, String doc) {
+        OkHttpClient myClient = new OkHttpClient();
+        String url = "http://getcitydetails.geobytes.com/GetCityDetails?fqcn=" + city;
+        Request req = new Request.Builder().url(url).build();
+        Response res = null;
+        try {
+            res = myClient.newCall(req).execute();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Object object = null;
+        myParser = new org.json.simple.parser.JSONParser();
+        try {
+            try {
+                object = myParser.parse(res.body().string());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (object != null) {
+            String capital = "", country = "", coin = "";
+            String population = "";
+            capital = (String) ((JSONObject) object).get("geobytescity");
+            country = (String) ((JSONObject) object).get("geobytescountry");
+            population = (String) ((JSONObject) object).get("geobytespopulation");
+            coin = (String) ((JSONObject) object).get("geobytescurrencycode");
+            if (!population.equals("")) {
+                population = divideNumbers(Long.parseLong(population));
+                citiesDetails.put(capital.toUpperCase(), new City(capital.toUpperCase(), country, coin, population));
+                cities.put(doc, citiesDetails.get(city));
+            }
+        }
+    }
 
     /**
      * @param population
@@ -150,6 +194,8 @@ public class CitiesIndexer {
             return toWrite;
         }
     }
+
+
     //checking if natural number
     private boolean isNaturalNumber(double number) {
         int x = (int) number;
@@ -160,26 +206,62 @@ public class CitiesIndexer {
         return true;
     }
 
-    //http://getcitydetails.geobytes.com/GetCityDetails?fqcn=USNYNYOR', true);
 
     public static void writeDocPostingFile() {
         try {
             FileWriter fw = new FileWriter("C:\\Users\\tzalach\\IdeaProjects\\SearchingApp\\src\\main\\java\\CitiesPosting.txt");
             BufferedWriter WriteFileBuffer = new BufferedWriter(fw);
             StringBuilder toWrite = new StringBuilder();
-            for(Map.Entry<String, City> entry : cities.entrySet()) {
+            for (Map.Entry<String, City> entry : cities.entrySet()) {
                 String key = entry.getKey();
                 City value = entry.getValue();
-                if(value!=null) {
+                if (value != null) {
                     toWrite.append(key + " " + value.getName() + "\n");
                 }
             }
             WriteFileBuffer.write(toWrite.toString());
             WriteFileBuffer.close();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private String OurReplace(String s, char[] targets, String replacement) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char check = s.charAt(i);
+            boolean contain = false;
+            for (int j = 0; !contain && j < targets.length; j++) {
+                if (check == targets[j]) {
+                    contain = true;
+                }
+            }
+            if (contain) {
+                sb.append(" ");
+            } else {
+                sb.append(check);
+            }
+        }
+        return sb.toString();
+    }
+
+    private static String[] mySplit(String str, String regex) {
+        Vector<String> result = new Vector<String>();
+        int start = 0;
+        int pos = str.indexOf(regex);
+        while (pos >= start) {
+            if (pos > start) {
+                result.add(str.substring(start, pos));
+            }
+            start = pos + regex.length();
+            //result.add(regex);
+            pos = str.indexOf(regex, start);
+        }
+        if (start < str.length()) {
+            result.add(str.substring(start));
+        }
+        String[] array = result.toArray(new String[0]);
+        return array;
     }
 }
 
