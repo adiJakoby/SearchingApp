a1f0ec4bf69f50fd8b66795ec1421efe5ea93dde
adiJakoby <adij94@gmail.com>
2018-12-01 13:26:56 +0200

    still working

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,31 +1,64 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        /**
+         * fxml loading
+         */
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent root = fxmlLoader.load(getClass().getResource("/sample.fxml").openStream());
+        Scene scene = new Scene(root, 600, 500);
+        //scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        primaryStage.setTitle("Retrieval Engine");
+        primaryStage.setScene(scene);
+        primaryStage.setMinHeight(465);
+        primaryStage.setMinWidth(580);
+        //view.setSubController();
+        //SetStageCloseEvent(primaryStage);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
+    /*
+    private void SetStageCloseEvent(Stage primaryStage) {
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent windowEvent) {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Exit");
+                alert.setHeaderText("Exit confirmation");
+                alert.setContentText("Are you sure you want to quit?");
+                Optional<ButtonType> result = alert.showAndWait();
+                if (result.get() == ButtonType.OK) {
+                    //model.stopServers();
+                    // ... user chose OK
+                    // Close program
+                } else {
+                    // ... user chose CANCEL or closed the dialog
+                    windowEvent.consume();
+                }
+            }
+        });
+    }*/
+
 
     public static void main(String[] args) throws IOException {
+        launch(args);
         DocsInformation docsInformation = new DocsInformation();
         String workingDir = System.getProperty("user.dir");
         System.out.println(workingDir);
@@ -37,7 +70,6 @@ public class Main extends Application {
         Indexer indexer = new Indexer();
         indexer.mergePostingFile();
         docsInformation.saveTheInformation(workingDir + "\\src\\main\\java\\");
-        //CitiesIndexer.writeDocPostingFile();
         Date date1 = new Date();
         System.out.println(dateFormat.format(date1));
     }
